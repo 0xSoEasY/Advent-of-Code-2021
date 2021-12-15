c7db5f53c17c23e396d5e6bdafdb0e82d873595f
0xSoEasY <julien@rouillier.fr>
2021-12-15 19:33:14 +0100

    Upgrading 'new_day.sh'

--- new_day.sh
+++ new_day.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
 
+export session_id="YOUR_SESSION_ID_HERE"
+
 if [ "$#" -ne 1 ]; then
     echo "Usage: ./new_day.sh <day_number>"
     exit 1
+
+elif [ $session_id == "YOUR_SESSION_ID_HERE" ]; then
+    echo "[-] Edit the script and modify your session_id"
+    exit 2
 fi
 
-export session_id="YOUR_SESSION_ID_HERE"
 export challenge_name=$(curl -s https://adventofcode.com/2021/day/$1 | grep "\-\-\- Day" | sed 's/.*<h2>.*: //' | sed 's/ \-\-\-<\/h2>.*//' | sed 's/ /_/')
 export folder_name="$1_$challenge_name"
 
-printf "[+] Creating folder '%s'\n" $folder_name
+printf "[!] Creating folder './%s/'\n" $folder_name
 mkdir $folder_name
 cd $folder_name
 
+echo "[!] Compying the template 'solve.c' script"
 cp ../template.c solve.c
 
+echo "[!] Fetching the 'input.txt' file"
 curl -s "https://adventofcode.com/2021/day/$1/input" -H "cookie: session=$session_id" > input.txt
+
+echo "[+] Done !"
